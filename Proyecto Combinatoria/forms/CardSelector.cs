efd6ac12835bd36c0bfb3e0f5a5feaaf145d665a
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2022-06-23 17:11:26 -0300

    CardSelector v1

--- Proyecto Combinatoria/forms/CardSelector.cs
+++ Proyecto Combinatoria/forms/CardSelector.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,34 @@ namespace Proyecto_Combinatoria
 {
     public partial class CardSelector : Form
     {
+        private Card card1;
+        private Card card2;
+
+        public Card Card1
+        {
+            get => card1;
+  
+            set
+            {
+                if (this.card1 == null)
+                {
+                    this.card1 = value;
+                }
+            }
+        }
+
+        public Card Card2
+        {
+            get => this.card2;
+            set
+            { 
+                if (card2 == null)
+                {
+                    this.card2 = value;
+                }
+            } 
+        }
+
         public CardSelector()
         {
             InitializeComponent();
@@ -22,12 +51,44 @@ namespace Proyecto_Combinatoria
 
         }
 
-        private void elegirPaloComboBox_SelectedIndexChanged(object sender, EventArgs e)
+
+        private void carta1_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, carta1.ClientRectangle,
+                Color.Yellow, 4, ButtonBorderStyle.Solid,
+                Color.Yellow, 4, ButtonBorderStyle.Solid,
+                Color.Yellow, 4, ButtonBorderStyle.Solid,
+                Color.Yellow, 4, ButtonBorderStyle.Solid
+                );
+        }
+
+
+        private void carta2_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, carta2.ClientRectangle,
+                Color.Yellow, 4, ButtonBorderStyle.Solid,
+                Color.Yellow, 4, ButtonBorderStyle.Solid,
+                Color.Yellow, 4, ButtonBorderStyle.Solid,
+                Color.Yellow, 4, ButtonBorderStyle.Solid
+                );
+        }
+
+        private void panelResultados_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, panelResultados.ClientRectangle,
+               Color.Yellow, 4, ButtonBorderStyle.Solid,
+               Color.Yellow, 4, ButtonBorderStyle.Solid,
+               Color.Yellow, 4, ButtonBorderStyle.Solid,
+               Color.Yellow, 4, ButtonBorderStyle.Solid
+               );
+        }
+
+        private void jugadaColor_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void carta1_Paint(object sender, PaintEventArgs e)
+        private void tituloJugadas_Click(object sender, EventArgs e)
         {
 
         }
